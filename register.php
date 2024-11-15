024d898df270731c62d661df8372fba20e75d18b
wvv999 <wesley3viana@gmail.com>
2024-11-15 20:35:07 -0300

    *

--- register.php
+++ register.php
@@ -87,9 +87,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 
                 <button type="submit" class="btn btn-primary w-100">Cadastrar</button>
             </form>
+            <div>
             <a href="javascript:history.back();" class="btn btn-outline-danger logout-btn">
                 Voltar
-            </a>   
+            </a>
+            </div>
             
 
         </div>
