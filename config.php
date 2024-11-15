7093e2c55fb7fb31def2461707768c43facb77b6
wvv999 <wesley3viana@gmail.com>
2024-11-15 20:51:35 -0300

    seg

--- config.php
+++ config.php
@@ -29,8 +29,3 @@ class Database {
 session_start();
 require_once 'config.php';
 
-// Verifica se está logado (proteção da página)
-if(!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit;
-}
