0c42d58b5d5ae4b99116431c6efe46e8820f98e5
wvv999 <wesley3viana@gmail.com>
2024-11-15 17:51:46 -0300

    teste

--- index.php
+++ index.php
@@ -38,8 +38,7 @@ $remembered_user = isset($_COOKIE['remember_user']) ? $_COOKIE['remember_user'] 
 <body>
     <div class="container">
         <div class="login-container">
-            <h2 class="text-center mb-4">Loginnnn</h2>
-            <h1>Teste</h1>
+            <h2 class="text-center mb-4">Login</h2>
             
             <form method="POST" action="">
                 <div class="mb-3">
