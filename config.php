eb4f08bbe1b04d1d495e2bf5ddf252e256ed9f49
wvv999 <wesley3viana@gmail.com>
2024-11-15 21:27:52 -0300

    test

--- config.php
+++ config.php
@@ -26,3 +26,37 @@ class Database {
         return $this->conn;
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Config</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+
+        .container {
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <div>
+            <a href="javascript:history.back();" class="btn btn-outline-danger logout-btn w-100 sair">
+                Voltar
+            </a>
+            </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
