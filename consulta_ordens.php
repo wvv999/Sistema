b1a664fbf7eb3a609d6241d54e02a6cd5e510bcd
wvv999 <wesley3viana@gmail.com>
2024-11-17 14:03:28 -0300

    1

--- consulta_ordens.php
+++ consulta_ordens.php
@@ -51,7 +51,7 @@ try {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <style>
         .body{
-            padding:5px;
+            padding:50px;
         }
         .container {
             padding-top: 2rem;
