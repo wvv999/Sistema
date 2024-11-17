b959fdeac7d72a2763e1682643c8eaae45f3bbeb
wvv999 <wesley3viana@gmail.com>
2024-11-17 20:25:01 -0300

    1

--- view_order.php
+++ view_order.php
@@ -47,7 +47,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ordem de Serviço #<?php echo $order['id']; ?></title>
+    <title>Ordem de Serviço <?php echo $order['id']; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <style>
@@ -222,7 +222,7 @@ try {
                 </div>
                 <div>
                     <span class="header-label">Ordem:</span>
-                    <span class="header-value">#<?php echo $order['id']; ?></span>
+                    <span class="header-value"><?php echo $order['id']; ?></span>
                 </div>
             </div>
             <div class="header-row">
