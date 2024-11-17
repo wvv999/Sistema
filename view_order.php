c7322c91858eb6ff5ec4a4dab7db7bf29f885a05
wvv999 <wesley3viana@gmail.com>
2024-11-17 16:52:53 -0300

    teste

--- view_order.php
+++ view_order.php
@@ -276,7 +276,7 @@ try {
 
         <div>
             <div class="section-title">Defeito Reclamado</div>
-            <div class="reported-issue w-80">
+            <div class="reported-issue w-75">
                 <?php echo htmlspecialchars($order['reported_issue']); ?>
             </div>
         </div>
