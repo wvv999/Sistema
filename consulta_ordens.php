904735b0e422e4864a95395127eb3e52f52519b9
wvv999 <wesley3viana@gmail.com>
2024-11-17 15:49:22 -0300

    front

--- consulta_ordens.php
+++ consulta_ordens.php
@@ -124,10 +124,10 @@ try {
                                 <th>Telefone 2</th>
                                 <th>Data de Abertura</th>
                                 <th>Data de Entrega</th>
-                                <th>Problema Relatado</th>
+                                <!-- <th>Problema Relatado</th> -->
                                 <!-- <th>Acessórios</th> -->
-                                <th>Senha do Dispositivo</th>
-                                <th>Senha Padrão</th>
+                                <!-- <th>Senha do Dispositivo</th> -->
+                                <!-- <th>Senha Padrão</th> -->
                                 <th>Ações</th>
                             </tr>
                         </thead>
@@ -140,10 +140,10 @@ try {
                                     <td><?= htmlspecialchars($order['phone2']) ?></td>
                                     <td><?= date('d/m/Y H:i', strtotime($order['opening_date'])) ?></td>
                                     <td><?= $order['delivery_date'] ? date('d/m/Y', strtotime($order['delivery_date'])) : '-' ?></td>
-                                    <td><?= nl2br(htmlspecialchars($order['reported_issue'])) ?></td>
+                                    <!-- <td><?= nl2br(htmlspecialchars($order['reported_issue'])) ?></td> -->
                                     <!-- <td><?= htmlspecialchars($order['accessories']) ?></td> -->
-                                    <td><?= htmlspecialchars($order['device_password']) ?></td>
-                                    <td><?= htmlspecialchars($order['pattern_password']) ?></td>
+                                    <!-- <td><?= htmlspecialchars($order['device_password']) ?></td> -->
+                                    <!-- <td><?= htmlspecialchars($order['pattern_password']) ?></td> -->
                                     <td>
                                         <a href="view_order.php?id=<?= $order['id'] ?>" class="btn btn-primary view-btn">
                                             <i class="bi bi-eye"></i> Ver
