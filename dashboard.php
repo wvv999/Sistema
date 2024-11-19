786cb05bad4c9a56c9110a6e415abb74f9123c1e
wvv999 <wesley3viana@gmail.com>
2024-11-19 01:13:07 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -197,8 +197,16 @@ if(!isset($_SESSION['user_id'])) {
                                 </div>
                                 <div class="d-flex align-items-center gap-3">
                                     <small class="text-muted">{$createdAt}</small>
+
+
+                                    <!-- Indicador da situação -->
                                     <button class="btn btn-sm btn-outline-primary btn-view-order" onclick="event.stopPropagation(); window.location='view_order.php?id={$order['id']}'">
-                                        <i class="bi bi "></i> Situação <div></div>
+                                        <i class="bi bi-eye"></i> Situação
+                                    </button>
+
+
+                                    <!-- botão de ver -->
+                                    <button class="btn btn-sm btn-outline-primary btn-view-order" onclick="event.stopPropagation(); window.location='view_order.php?id={$order['id']}'">
                                         <i class="bi bi-eye"></i> Ver
                                     </button>
                                 </div>
