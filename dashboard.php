18021dc59f284991f3387d0a8b54349c44193010
wvv999 <wesley3viana@gmail.com>
2024-11-19 00:52:53 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -148,7 +148,7 @@ if(!isset($_SESSION['user_id'])) {
                 <div class="col-md-6">
                     <a href="clientes.php" class="btn btn-outline-success w-100 nav-button">
                         <i class="bi bi-person-lines-fill"></i>
-                        Cadastrar Clientes
+                        Cadastro de Clientes
                     </a>
                 </div>
 
