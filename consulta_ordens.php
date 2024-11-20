36b3efe3cc113a5fcd146f0a778374bd360a2be2
wvv999 <wesley3viana@gmail.com>
2024-11-20 12:28:01 -0300

    teste

--- consulta_ordens.php
+++ consulta_ordens.php
@@ -141,11 +141,7 @@ try {
                                     <td><?= htmlspecialchars($order['phone2']) ?></td>
                                     <td><?= date('d/m/Y', strtotime($order['opening_date'])) ?></td>
                                     <td><?= $order['delivery_date'] ? date('d/m/Y', strtotime($order['delivery_date'])) : '-' ?></td>
-                                    <td><?= htmlspecialchars($order['status']) ?></td>
-                                    <!-- <td><?= nl2br(htmlspecialchars($order['reported_issue'])) ?></td> -->
-                                    <!-- <td><?= htmlspecialchars($order['accessories']) ?></td> -->
-                                    <!-- <td><?= htmlspecialchars($order['device_password']) ?></td> -->
-                                    <!-- <td><?= htmlspecialchars($order['pattern_password']) ?></td> -->
+                                    <td><?= OrderStatus::getStatusButton($order['status']) ?></td>
                                     <td>
                                         <a href="view_order.php?id=<?= $order['id'] ?>" class="btn btn-primary view-btn">
                                             <i class="bi bi-eye"></i> Ver
