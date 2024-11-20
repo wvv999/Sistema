44744ebe2412d82cfd929249ca1097acc503e0d9
wvv999 <wesley3viana@gmail.com>
2024-11-20 20:57:30 -0300

    teste

--- gestao.php
+++ gestao.php
@@ -22,6 +22,7 @@ require_once 'functions.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" href="css/status-styles.css">
     <style>
         .container { padding-top: 2rem; padding-bottom: 2rem; }
         
