869329043b01a71009f0bdfa7e8c2d86d3c2dff2
wvv999 <wesley3viana@gmail.com>
2024-11-20 21:06:42 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,6 @@ if(!isset($_SESSION['user_id'])) {
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/status-styles.css">
     <style>
         .container { padding-top: 2rem; padding-bottom: 2rem; }
         
