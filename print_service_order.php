d8cd7c4d26ce0856ce67d5aecde3d50e8ef3468f
wvv999 <wesley3viana@gmail.com>
2024-11-21 23:00:17 -0300

    teste

--- print_service_order.php
+++ print_service_order.php
@@ -145,7 +145,7 @@ $delivery_date = date("d/m/Y", strtotime($order['delivery_date']));
         }
         .brush-script {
             font-family: "Brush Script MT", "Brush Script Std", cursive;
-            font-size: 20px;
+            font-size: 60px;
         }
     </style>
 </head>
