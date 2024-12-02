5abdef5e4fc1fcad416585f670c027e6974fe088
wvv999 <wesley3viana@gmail.com>
2024-12-02 20:37:06 -0300

    teste

--- gestao.php
+++ gestao.php
@@ -29,6 +29,9 @@ try {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <style>
+        body::-webkit-scrollbar{
+            display: none;
+        }
         :root {
             --primary-color: #4a6fff;
             --secondary-color: #f8f9fa;
