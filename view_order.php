7c52485c01f44801255bf7a8a54689f3bc75a9fa
wvv999 <wesley3viana@gmail.com>
2024-12-06 00:11:22 -0300

    teste

--- view_order.php
+++ view_order.php
@@ -111,7 +111,7 @@ try {
             display: flex;
             flex-direction: column;
             min-height: calc(100vh - 40px);
-            scale: 95%;
+            scale: 85%;
         }
 
         /* Order info styles */
