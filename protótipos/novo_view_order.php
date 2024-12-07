365d1495b75d066a5efd98048b04fffde7484eaa
wvv999 <wesley3viana@gmail.com>
2024-12-07 00:01:35 -0300

    teste

--- protótipos/novo_view_order.php
+++ protótipos/novo_view_order.php
@@ -4,7 +4,7 @@ require_once './config.php';
 
 
 if(!isset($_SESSION['user_id'])) {     
-    header("Location: index.php");     
+    header("Location: ./index.php");     
     exit; 
 }  
 
--- protótipos/view_order.php
+++ protótipos/view_order.php
@@ -5,7 +5,7 @@ require_once './config.php';
 
 
 if(!isset($_SESSION['user_id'])) {     
-    header("Location: index.php");     
+    header("Location: ./index.php");     
     exit; 
 }  
 
