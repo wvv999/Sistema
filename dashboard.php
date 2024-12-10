3c6c92cc9d6962f3c4ecc8f924a7160fdec2a1c1
wvv999 <wesley3viana@gmail.com>
2024-12-10 00:49:03 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -132,7 +132,7 @@ if(!isset($_SESSION['user_id'])) {
         }
         
         .não-iniciada {background: #e74c3c;}
-        .em-andamento {background: #f39c12; color: red}
+        .em-andamento {background: #f39c12;}
         .concluída {background: #27ae60;}
         .pronto-e-avisado {background: #3498db; color: white}
         .entregue {background: #2c3e50; color: white}
