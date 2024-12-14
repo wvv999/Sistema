739ec76fe88a818f1482607cdafe49797edd85c4
wvv999 <wesley3viana@gmail.com>
2024-12-14 20:25:18 -0300

    teste

--- novo_view_order.php
+++ novo_view_order.php
@@ -593,26 +593,26 @@ try {
 
         </div>
     </div>
-    <div class="mid-content">
-                <div class="section-title">
-                    <i class="bi bi-clipboard-data"></i> Laudo Técnico
-                </div>
-                    <div class="technical-report">
-                        <div class="technical-notes">
-                            <textarea id="technicalNotes" rows="6" readonly><?php echo $textareaContent; ?></textarea>
-                            
-                            <div class="add-note-form">
-                                <div class="input-group">
-                                    <textarea id="newNote" 
-                                            rows="1"
-                                            placeholder="Digite sua nota técnica..."
-                                            data-autoresize></textarea>
-                                    <button onclick="addNote()" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Adicionar</button>
-                                </div>
-                            </div>
+        <div class="mid-content">
+            <div class="section-title">
+                <i class="bi bi-clipboard-data"></i> Laudo Técnico
+            </div>
+            <div class="technical-report">
+                <div class="technical-notes">
+                    <textarea id="technicalNotes" rows="6" readonly><?php echo $textareaContent; ?></textarea>
+                    <div class="add-note-form">
+                        <div class="input-group">
+                            <textarea id="newNote" 
+                                rows="1"
+                                placeholder="Digite sua nota técnica..."
+                                data-autoresize>
+                            </textarea>
+                            <button onclick="addNote()" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Adicionar</button>
                         </div>
                     </div>
+                </div>
             </div>
+        </div>
             <div class="content-right">
                 <div class="side-panel">
                     <!-- Status e Ações -->
