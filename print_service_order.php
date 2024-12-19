af58431d46f2107df6e20c5b4294b81de7edf9e7
wvv999 <wesley3viana@gmail.com>
2024-12-19 01:44:52 -0300

    teste

--- print_service_order.php
+++ print_service_order.php
@@ -385,7 +385,7 @@ $delivery_date = date("d/m", strtotime($order['delivery_date']));
                     </div>
                     
                     <div class="signature-line">
-                        <div style="color: gray;"><?php echo htmlspecialchars($order['client_name']); ?></div>
+                        <div><?php echo htmlspecialchars($order['client_name']); ?></div>
                     </div>
                 </div>
             </div>
