2579da826759821fb9cd55892cf0adc3505748c0
wvv999 <wesley3viana@gmail.com>
2024-12-19 01:35:33 -0300

    teste

--- print_service_order.php
+++ print_service_order.php
@@ -404,7 +404,7 @@ $delivery_date = date("d/m", strtotime($order['delivery_date']));
                         <div class="company-info">WhatsApp: (51) 99538-4838</div>
                     </div>
                     <div class="header-right">
-                        <div>OS Nº <?php echo str_pad($order['id'], 5, "0", STR_PAD_LEFT); ?></div>
+                        <div><strong><?php echo str_pad($order['id'], 5, "0", STR_PAD_LEFT); ?></strong></div>
                         <div class="delivery-date">Entrega: <?php echo $delivery_date; ?></div>
                     </div>
                 </div>
