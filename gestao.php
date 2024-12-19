6a34f15b0dd9250c20fce063f03f27973d779040
wvv999 <wesley3viana@gmail.com>
2024-12-19 01:12:22 -0300

    teste

--- gestao.php
+++ gestao.php
@@ -141,7 +141,7 @@ try {
         }
         .teste{
             padding: 6px;
-            border-radius: 0px !important;
+            border-radius: 5px !important;
         }
     </style>
 </head>
