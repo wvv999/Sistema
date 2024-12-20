380351be132534bb3850d92e0ed5a1dd144b63ef
Wvv999 <wesley3viana@gmail.com>
2024-12-20 13:38:52 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION['user_id'])) {
         .container { padding-top: 2rem; padding-bottom: 2rem; }
         
         .dashboard-container {
-            background-color: red;
+            background-color: white;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0,0,0,0.1);
             padding: 20px;
