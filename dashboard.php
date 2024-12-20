8fbea5575c542f8856ac504b0e78f7d1163be963
Wvv999 <wesley3viana@gmail.com>
2024-12-20 13:58:37 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -31,6 +31,10 @@ if(!isset($_SESSION['user_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <style>
+        body{
+            padding: 20px;
+        }
+
         .container { padding-top: 2rem; padding-bottom: 2rem; }
         
         .dashboard-container {
