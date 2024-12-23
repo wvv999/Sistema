c99dd8f6dcd79aaf0f39c2a1c99bc06600cd6920
wvv999 <wesley3viana@gmail.com>
2024-12-23 04:11:35 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -384,7 +384,7 @@ if(!isset($_SESSION['user_id'])) {
                                             <small class="text-muted d-block">Cliente: {$clientName}</small>
                                         </div>
                                         <div class="d-flex align-items-center gap-3">
-                                            <small class="text-muted">{echo $horas}</small>
+                                            <small class="text-muted">$horas</small>
                                             {$statusButton}
 
                                         </div>
