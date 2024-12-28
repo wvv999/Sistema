f0b367589896ef0897ff3ddf612a02886f029bca
wvv999 <wesley3viana@gmail.com>
2024-12-27 23:03:26 -0300

    teste

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ class Database {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci; SET time_zone = '-03:00'"
             ];
 
             $this->conn = new PDO($dsn, $this->user, $this->password, $options);
