33cc8ba8b0a84b5395655ece7be14d463ff72976
wvv999 <wesley3viana@gmail.com>
2025-01-26 21:47:07 -0300

    teste

--- dashboard.php
+++ dashboard.php
@@ -266,6 +266,7 @@ if(!isset($_SESSION['user_id'])) {
             background: white;
             box-shadow: 0px 0px 8px -2px rgba(0,0,0,0.75);
             border-radius: 10px;
+            color:black;
         }
         .botao > a{
             width: 100%;
