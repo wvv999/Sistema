1ace8ea357b2a37ca440fb52282cc870798e1566
wvv999 <wesley3viana@gmail.com>
2025-01-28 23:03:33 -0300

    teste

--- novo_novo_view_order.php
+++ novo_novo_view_order.php
@@ -141,7 +141,7 @@ try {
                     <div class="info-label">
                         <i class="bi bi-person"></i> Nome do Cliente
                     </div>
-                    <div class="info-value">asdasdasdasdasdasda<?php echo htmlspecialchars($order['client_name']); ?></div>
+                    <div class="info-value"><?php echo htmlspecialchars($order['client_name']); ?></div>
 
 
 
