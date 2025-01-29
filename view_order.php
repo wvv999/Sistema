518adec95f34bccbadcc1cfdc0784906099e98e8
wvv999 <wesley3viana@gmail.com>
2025-01-29 02:01:30 -0300

    teste

--- view_order.php
+++ view_order.php
@@ -767,12 +767,13 @@ try {
             loadOrderHistory(); // Carrega o histórico
             historyModal.show(); // Mostra o modal
         });
-    
+    </script>
 
-    
+    <!-- Container para notificações toast -->
+    <div class="toast-container"></div>
 
-    
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
         // Inicializa todos os tooltips
         //var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
         //var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
