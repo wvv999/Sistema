65a5635b1dab8ba9b202f16f14d999fda0ad6612
wvv999 <wesley3viana@gmail.com>
2025-01-29 02:00:53 -0300

    teste

--- view_order.php
+++ view_order.php
@@ -1010,7 +1010,6 @@ try {
             updateAuthButtonOnLoad();
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
