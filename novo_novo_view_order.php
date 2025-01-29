93a79e811fdee5a5a902e7f71d388f590e68fcb4
wvv999 <wesley3viana@gmail.com>
2025-01-28 23:06:35 -0300

    teste

--- novo_novo_view_order.php
+++ novo_novo_view_order.php
@@ -74,6 +74,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ordem de Serviço <?php echo $order['id'] ?> </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <style>
         body {
             background-color: #f5f6fa;
@@ -137,11 +138,20 @@ try {
                     <h4 class="mb-3">
                         Ordem número: <?php echo str_pad($order['id'], STR_PAD_RIGHT); ?>
                     </h4>
-
                     <div class="info-label">
                         <i class="bi bi-person"></i> Nome do Cliente
                     </div>
-                    <div class="info-value"><?php echo htmlspecialchars($order['client_name']); ?></div>
+                    <div class="info-value">
+                        <?php echo htmlspecialchars($order['client_name']); ?>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="info-label">
+                            <i class="bi bi-laptop"></i> Modelo
+                        </div>
+                        <div class="info-value"><?php echo htmlspecialchars($order['device_model']); ?></div>
+                    </div>
+
+
 
 
 
